50536e749cad9b60ca7c8807414ab887c683ef11
lucas javier <santilujavier@gmail.com>
2023-08-29 21:23:22 -0300

    quite archivos de las clases

--- 01-clase/template-strings.js
+++
@@ -1,8 +0,0 @@
-const nombre = "Lucas";
-const edad = 19;
-const frase = "Hola soy " + nombre + " y tengo " + edad + " años";
-console.log(frase);
-
-// template string ${var}
-const frase2 = `Hola soy ${nombre} y tengo ${edad} años`;
-console.log(frase2);
--- 04-clase/settimeout.js
+++
@@ -1,4 +0,0 @@
-// https://www.exercism.org/tracks/javascript/concepts
-// https://www.hackerrank.com
-// https://www.leetcode.com
-// 00:12:00
